2377b1ec61e28bd0c8d80c45fada5fcff48c73f9
hao <329114429@qq.com>
2020-02-17 17:37:17 +0800

    信用卡

--- repay/repay_money.py
+++ repay/repay_money.py
@@ -1,5 +1,18 @@
 # 信用卡还款的详细,然后根据信用卡的还款日期转换使用
 
 
-class Repay():
-    pass
+class Credit_card():
+
+    # 信用卡总额, 可借用金额, 利率
+    def __init__(self, rental, lend_money, rate):
+        self.rental = rental
+        self.lend_money = lend_money
+        self.rate = rate
+
+    # 借钱
+    def lend(self):
+        pass
+
+    # 还钱
+    def repay(self):
+        pass
