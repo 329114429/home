90425c26be6e40ade10f3bb060b3d6e9dff5fac9
hao <329114429@qq.com>
2020-02-18 16:48:58 +0800

    信用卡

--- repay/repay_money.py
+++ repay/repay_money.py
@@ -10,9 +10,24 @@ class Credit_card():
         self.rate = rate
 
     # 借钱
-    def lend(self):
-        pass
+    def lend(self, lend_money):
+        self.lend_money = lend_money
+        return lend_money
 
     # 还钱
-    def repay(self):
-        pass
+    def repay(self, repay_money):
+        self.repay_money = repay_money
+
+        total_repay_money = self.repay_money + self.lend_money * self.rate
+
+        return total_repay_money
+
+    # 还款日期
+    def repay_date(self, repay_date):
+        self.repay_date = repay_date
+        return repay_date
+
+    # 借款日期
+    def lend_date(self, lend_date):
+        self.lend_date = lend_date
+        return lend_date
