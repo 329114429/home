55d4042d242ed710968e1c618f54f611f0cb7385
hao <329114429@qq.com>
2020-02-26 16:34:08 +0800

    时间戳相减再转换成天数

--- repay/card.py
+++ repay/card.py
@@ -5,11 +5,11 @@ def IGBG_run():
     IGBG = Card(54000, 25000, 0.005)
 
     # 借钱日期
-    lend_day, lend_timeStamp = IGBG.lend_day("2020-2-12")
+    lend_day, lend_timeStamp = IGBG.lend_day("2020-2-11")
     print(lend_day, lend_timeStamp)
 
     # 还钱日期
-    repay_day, repay_timeStamp = IGBG.repay_day("2020-2-14")
+    repay_day, repay_timeStamp = IGBG.repay_day("2020-3-12")
     print(repay_day, repay_timeStamp)
 
     days, days_money = IGBG.day_rate()
